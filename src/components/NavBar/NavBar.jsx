ff692cc0cd5ce972b7bdca579b27f95b2bf168d4
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-10-07 19:40:32 -0300

    Remove catalog link from NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,22 +4,17 @@ import logo from '../../assets/img/logo.png'
 
 export const NavBar = () => {
     return (
-        <Navbar expand="lg" className="bg-first fs-4">
-            <div className="container">
-              <Navbar.Brand href="/"><img src={logo} alt="Logo de Ovillo Feliz" className='navbar-img' /><span className='m-2 fs-4 nav-title'>Ovillo Feliz</span></Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                      <Nav className="ms-auto nav-gap align-items-center">
-                        <NavDropdown className="nav-link" title="Catálogo">
-                          <NavDropdown.Item className="nav-link" href="#">Ovillos</NavDropdown.Item>
-                          <NavDropdown.Item className="nav-link" href="#">Hilos</NavDropdown.Item>
-                          <NavDropdown.Item className="nav-link" href="#">Accesorios</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link className="nav-link" href="#">Iniciar sesión</Nav.Link>
-                        <Nav.Link className="nav-link" href="#">Carrito</Nav.Link>
-                      </Nav>
-                  </Navbar.Collapse>
-            </div>
-        </Navbar>
+      <Navbar expand="lg" className="bg-first fs-4">
+        <div className="container">
+          <Navbar.Brand href="/"><img src={logo} alt="Logo de Ovillo Feliz" className='navbar-img' /><span className='m-2 fs-4 nav-title'>Ovillo Feliz</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-gap align-items-center">
+              <Nav.Link className="nav-link" href="#">Iniciar sesión</Nav.Link>
+              <Nav.Link className="nav-link" href="#">Carrito</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
     );
 }
