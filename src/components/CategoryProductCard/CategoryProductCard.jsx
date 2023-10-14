c14cd2b8ba5dcf22ddd7038e2bb04ed3ac499612
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-10-14 10:34:05 -0300

    Format price of product

--- src/components/CategoryProductCard/CategoryProductCard.jsx
+++ src/components/CategoryProductCard/CategoryProductCard.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom';
 import './categoryProductCard.css';
+import { useContext } from 'react';
+import CartContext from '../../context/CartContext/CartContext';
 
 export const CategoryProductCard = ({ title, image, price, name }) => {
+
+  const cartCtx = useContext(CartContext);
+  const { formatPrice } = cartCtx;
+
   return (
     <>
       <div className='col mb-4'>
@@ -9,7 +15,7 @@ export const CategoryProductCard = ({ title, image, price, name }) => {
           <Link to={`/productos/${name}`}><img className='img-fluid cat-product-card-img' src={image} alt={title} /></Link>
             <Link to={`/productos/${name}`} className='cat-product-link'><div className='card-body'>
               <h5 className='cat-card-title fs-4 mt-4'>{title}</h5>
-              <p className='cat-card-price fs-4'>${price}</p>
+              <p className='cat-card-price fs-4'>{formatPrice.format(price)}</p>
             </div></Link>
         </div>
       </div>
