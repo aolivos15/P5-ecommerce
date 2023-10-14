6389840045c1c9c546e85410a5a1823177935b2f
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-10-14 10:24:41 -0300

    Modify size of product card

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -7,7 +7,7 @@ export const FeaturedProducts = () => {
   return (
     <>
       <div className="container-fluid py-5">
-        <h2 className="resp-title text-center">Productos destacados</h2>
+        <h2 className="resp-title text-center mb-5 fs-1 fw-bold">Productos destacados</h2>
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 justify-content-evenly">
             <FeaturedProductCard name='Básica' img={prod_basica} price='$1.990' />
             <FeaturedProductCard name='Súper' img={prod_super} price='$2.390' />
